572033ba82f7144b9b192b282b441142b8e99842
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-18 19:23:39 +0100

    Create 0-minoperations.py

---
+++ 0x00-python_variable_annotations/0-minoperations.py
@@ -0,0 +1,25 @@
+ #!/usr/bin/python3
+"""
+method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations (copy, paste)
+    """
+    len_H = 1
+    sum_of_copied_H = 0
+    sum_of_operations = 0
+
+    while len_H < n:
+        if n % len_H == 0:
+            sum_of_operations += 2
+            sum_of_copied_H = len_H
+        else:
+            total_operations += 1
+          
+        len_H += sum_of_copied_H
+
+    return total_operations
