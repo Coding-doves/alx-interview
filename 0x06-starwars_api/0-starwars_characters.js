82910e2aef9860264b21351234d5b3df4d571994
Ada <obenedicta4.@gmail.com>
2024-02-16 07:31:42 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,38 +2,28 @@
 // starwars api
 
 const request = require('request'); // request
-
 const movieId = process.argv[2];
 
-// if (!movieId) {
-//   console.log("Please provide a movie ID.");
-//   process.exit(1);
-// }
-
-// movieId
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-function starwarsApi(url){
-  return new Promise( (res, rej) => {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        res(JSON.parse(body));
-      } else {
-        rej(error);
-      }
-    });
-  });
-}
-
-// entry point
-
-async function main (apiUrl) {
-  const result = await starwarsApi(apiUrl);
-
-  for (const i of result.characters) {
-    const n = await starwarsApi(i);
-    console.log(n.name);
+request.get(apiUrl, function (error, response, body) {
+  if (!error) {
+    const urlChar = JSON.parse(body).characters;
+    const len = urlChar.length;
+    starwarsApi(0, urlChar[0], urlChar, len); 
   }
-}
+})
 
-main(apiUrl);
+function starwarsApi(k, url, char, len) {
+  if (k === len) {
+    return
+  }
+
+  request.get(url, function (err, res, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      k++;
+      starwarsApi(k, char[k], char, len);
+    }
+  })
+}
