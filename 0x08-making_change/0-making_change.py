91ec57f72d8c4dc7143b305c39c07d0ab47758ef
Ada <obenedicta4.@gmail.com>
2024-02-28 00:36:38 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+making change
+
+testcase
+# print(makeChange([1, 2, 25], 37))
+# print(makeChange([1256, 54, 48, 16, 102], 1453))
+'''
+
+
+def makeChange(coins, total) -> int:
+    '''
+    Finding smallest amount
+    to change.
+    Sort and Reverse coins list
+    Loop through, div total by each coin
+    Add the answer -> change += total / one_coin
+    New total = total % one_coin
+    return change if total == 0
+    '''
+    change = 0
+
+    sort_n_reverse = sorted(coins, reverse=True)
+
+    for one_coin in sort_n_reverse:
+        if total <= 0:
+            return change
+
+        change += total // one_coin
+        total = total % one_coin
+
+    return -1
