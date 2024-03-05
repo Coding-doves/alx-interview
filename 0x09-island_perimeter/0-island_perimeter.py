eeee9c255bbf87254e56eb7b4fe79302ecca15f8
Ada <obenedicta4.@gmail.com>
2024-03-05 19:45:12 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
-'''Island Perimeter'''
+''' Island Perimeter '''
 
 
 def island_perimeter(grid):
-    '''def island_perimeter'''
+    ''' island perimeter '''
+    perimeter = 0
+    row = len(grid)
+    col = len(grid[0])
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                # check left
+                if j - 1 < 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # check right
+                if j + 1 == col or grid[i][j + 1] == 0:
+                    perimeter += 1
+                # check top
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # check bottom
+                if i + 1 == row or grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
